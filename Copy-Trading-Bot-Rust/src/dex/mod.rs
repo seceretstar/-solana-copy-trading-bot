699d484e13973e33ce11246bdce9bd84f0673359
hitradian <hitechlan2000@gmail.com>
2025-02-21 07:42:01 -0500

    complete program data analyzing and now can't  bonding curve account

--- Copy-Trading-Bot-Rust/src/dex/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod pump;
-pub mod raydium;
--- sample/raytx/examples/jito_tip_accounts.rs
+++
@@ -1,8 +0,0 @@
-use anyhow::Result;
-use raytx::jito::api::{get_tip_accounts, TipAccountResult};
-#[tokio::main]
-async fn main() -> Result<()> {
-    let accounts: TipAccountResult = get_tip_accounts().await?.try_into()?;
-    println!("tip accounts: {:#?}", accounts);
-    Ok(())
-}
